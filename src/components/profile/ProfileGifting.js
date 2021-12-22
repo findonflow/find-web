ac759c7f7b2ae523aa976635195988bbc192b637
KalonNeoMot <64910788+KalonNeoMot@users.noreply.github.com>
2021-12-22 00:09:00 +0000

    updated the validation for gift and fund

--- src/components/profile/ProfileGifting.js
+++ src/components/profile/ProfileGifting.js
@@ -2,13 +2,22 @@ import { Button, Form, Col, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { handleSendNameToAddress,  handleSendNameToName} from "../../functions/txfunctions";
 import { useFormStatus } from "../../functions/DisabledState";
-
+import { useImmer } from "use-immer";
 export function ProfileGifting({ profileData }) {
 
     const [recipient, setRecipient] = useState("")
     const [name, setName] = useState("--please select a name--")
     const [error, setError] = useState("")
-
+    const [formValues, setFormValues] = useImmer([
+        {
+            id: "recipientName",
+            value: ""
+        },
+        {
+            id: "sendingName",
+            value: ""
+        }
+    ])
 
     const handleSubmit = (event) => {
         const form = event.currentTarget;
@@ -67,9 +76,32 @@ export function ProfileGifting({ profileData }) {
         }
     }
 
+    function addressValidationFormatting(e) {
+        setFormValues((draft) => {
+            const varVal = draft.find((varVal) => varVal.id === e.target.name);
+            varVal.value = e.target.value;
+            if(varVal.value.includes(".find"))
+            {
+                varVal.value = varVal.value.split(".find")[0]
+            }
+            //now validate
+            if (addressValidation(varVal.value) === "invalid") {
+                e.target.classList.add("is-invalid")
+                e.target.classList.remove("is-valid")
+            }
+            else {
+                e.target.classList.add("is-valid")
+                e.target.classList.remove("is-invalid")
+            }
+
+            setRecipient(varVal.value)
+        })
+
+        
+    }
+
+
     const addressValidation = (address) => {
-
-
         if(address.length >= 3)
         {
             if (address.match(/[^a-z0-9-]/g) !== null)
@@ -104,6 +136,29 @@ export function ProfileGifting({ profileData }) {
             
         }
 
+        function nameValidationFormatting(e) {
+            setFormValues((draft) => {
+                const varVal = draft.find((varVal) => varVal.id === e.target.name);
+                varVal.value = e.target.value;
+
+                //now validate
+                if (nameValidation(varVal.value)) {
+
+                    e.target.classList.add("is-valid")
+                    e.target.classList.remove("is-invalid")
+                    
+                }
+                else {
+                    e.target.classList.add("is-invalid")
+                    e.target.classList.remove("is-valid")
+                }
+    
+                setName(varVal.value)
+            })
+    
+            
+        }
+
     return(
         <div>
             <Form noValidate onSubmit={handleSubmit} className="formInputs">
@@ -117,7 +172,7 @@ export function ProfileGifting({ profileData }) {
                             <Form.Label className="idd2 p-0 m-0">Choose the name you would like to gift to someone else and send it either by their .find name or blocto wallet address.</Form.Label>
                             </Col>
                             <Col className="p-0 m-0" md="5" xs="12">
-                            <Form.Select required className="text-center mt-2 w-100" onChange={handleDropDownChange}>
+                            <Form.Select required className="text-center mt-2 w-100" name="sendingName" onChange={nameValidationFormatting}>
                             <option key="NoName" value="no name selected">--please select a name--</option>
                             {profileData.leases.map(item => {
                             return (<option key={item.name} value={item.name}>{item.name}</option>);
@@ -130,14 +185,14 @@ export function ProfileGifting({ profileData }) {
                     <Row className="m-0 mt-1">
                     <span className="idd mt-4">Who would you like to gift to?</span>
                         <Col md="8" xs="12">
-                        <Form.Control className="mt-2 w-100" required type="text" placeholder="Enter a .find name or 0xAddress" name="recipientName" onChange={handleRecipientChange}/>
+                        <Form.Control className="mt-2 w-100" required type="text" placeholder="Enter a .find name or 0xAddress" name="recipientName" onChange={addressValidationFormatting}/>
                         </Col>
                         <Col md="4" xs="12">
                         <Button className="mt-2 w-100 mb-2" variant="outline-dark" type="submit">Gift</Button>
                         </Col>  
                         <Row xs="12" md="8">
-                        {error.length > 0 &&
-                        <div className="errorText">{error}</div>}
+                        {/* {error.length > 0 &&
+                        <div className="errorText">{error}</div>} */}
                         </Row>
                     </Row>
                 </Row>
--- src/components/profile/ProfileSendFT.js
+++ src/components/profile/ProfileSendFT.js
@@ -2,37 +2,61 @@ import { Button, Form, Col, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { handleSendFungible } from "../../functions/txfunctions";
 import { useFormStatus } from "../../functions/DisabledState";
+import { useImmer } from "use-immer";
 
 export function ProfileSendFT({ profileData }) {
 
     const [sendFT, setSendFT] = useState("flow")
     const [error, setError] = useState("")
+    const [formValues, setFormValues] = useImmer([
+        {
+            id: "sendAmt",
+            value: 0
+        }
+    ])
 
     const handleSubmit = (event) => {
         const form = event.currentTarget;
         
         event.preventDefault();
-        
-        const amount = parseFloat(form.sendAmt.value).toFixed(2);
+        if(validateInput(form.sendAmt.value))
+        {
+            console.log(form.sendAmt.value)
+            const amount = parseFloat(form.sendAmt.value).toFixed(2);
 
-        if(validateInput(amount)){
             const name = profileData.lease.name
+            
+            
             handleSendFungible(event,name, amount, sendFT)
         }
 
     }
 
+    function validateInputFormatting(e) {
+        setFormValues((draft) => {
+            const varVal = draft.find((varVal) => varVal.id === e.target.name);
+            varVal.value = e.target.value;
+            //now validate
+            if (varVal.value <= 0) {
+                e.target.classList.add("is-invalid")
+                e.target.classList.remove("is-valid")
+            }
+            else {
+                e.target.classList.add("is-valid")
+                e.target.classList.remove("is-invalid")
+            }
+        })
+    }
+
     const validateInput = (amount) => {
-        if(amount > 0)
-        {
-            setError("")
-            return true
-        }
-        else
-        {
+
+        if (amount <= 0) {
             setError("Value must be greater than 0");
             return false
-            
+        }
+        else {
+            setError("")
+            return true
         }
 
     }
@@ -59,11 +83,11 @@ export function ProfileSendFT({ profileData }) {
                                 </Form.Select>  
                             </Col> 
                         </Row>
-                        <Form.Control required className="mt-3"type="number" name="sendAmt" />
+                        <Form.Control required className="mt-3" type="number" name="sendAmt" onChange={validateInputFormatting} />
                         <Row>
-                        {error.length > 0 &&
+                        {/* {error.length > 0 &&
                         <div className="errorText">{error}</div>
-                        }
+                        } */}
                     </Row>
                     </Form.Group>
                     

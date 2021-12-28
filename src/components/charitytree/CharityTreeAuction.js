4c8a1a239c7f187d612255eff6683164b7aa880d
Christian Miles <christian@neomotorcycles.co.uk>
2021-12-28 01:05:40 +0000

    updates to charity page

--- src/components/charitytree/CharityTreeAuction.js
+++ src/components/charitytree/CharityTreeAuction.js
@@ -1,66 +1,89 @@
-import { Col, Row, Image, Button } from "react-bootstrap"
+import { Col, Row, Image, Button, Container, Form } from "react-bootstrap"
 import { AuctionBox } from "./AuctionBox";
 import './charity-tree.css';
 export function CharityTreeAuction() {
 
     return (
-        <div>
-            
-            {/* This is the row for the Col containing NFTT image, Timer and the Col containing Description, auction buttons and gift options */}
-            <Row className="p-5">
-                <Col className="p-3 m-2 auction-box-background h-75" xs="12" md="6">
-                    <Row>
-                        {/* NFT IMAGE */}
-                        <Image className="p-3" src="/assets/img/charitytree/Community Tree.webp"></Image>
-                    </Row>
-                    
-                </Col>
+        <Container style={{ backgroundColor: "white" }} fluid>
+            <Container className="px-5">
 
-                <Col className=" m-2 h-100">
+                {/* This is the row for the Col containing NFTT image, Timer and the Col containing Description, auction buttons and gift options */}
+                <Row className="pt-5 ">
+                    <Col className="p-0 m-2" xs="12" md="auto">
+                        <Row className="justify-content-center">
+                            {/* NFT IMAGE */}
+                            <Image className=" p-0 shadow" src="/assets/img/charitytree/Community Tree.webp" style={{ maxWidth: "420px" }} rounded></Image>
+                        </Row>
+                    </Col>
 
-                <Row>
-                        {/* AUCTION TIMER */}
-                        <div className="w-100 charity-tree-timer">00:00:00</div>
-                    </Row>
+                    <Col className="" xs="12" md={{span: 6, offset: 1}}>
 
-                    <Row >
-                <div className=" pt-5 pb-3 w-75"><h1>Neo x Flowverse Community Charity Christmas Tree </h1></div>
-            </Row>
-                    <Row className="">
-                        {/* DESCRIPTION */}
-                       
-                        <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec lacus at dui auctor bibendum. Donec eget urna mi. Ut vel sagittis dolor. Phasellus nec neque porttitor, tincidunt tortor vel, venenatis est. Aenean maximus, tortor pulvinar iaculis tempus, velit arcu malesuada lectus, ac ullamcorper odio lacus nec arcu. Nam ac posuere arcu, sit amet feugiat quam. Duis congue eleifend nisl, a bibendum dui ullamcorper ut. Aenean quam quam, porttitor at tristique vel, tristique vitae ex. Duis pretium mattis felis, a cursus nisi venenatis id. Duis nec vehicula lectus. Maecenas et nisl in ex tristique mollis.</p></div>
-                    </Row>
-    
-                    <Row className="m-3 auction-box">
-                        {/* AUCTION buttons */}
-                        <div className="w-100 charity-tree-headers">Auction</div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec lacus at dui auctor bibendum. Donec eget urna mi. Ut vel sagittis dolor. Phasellus nec neque porttitor, tincidunt tortor vel, venenatis est. Aenean maximus, tortor pulvinar iaculis tempus, velit arcu malesuada lectus, ac ullamcorper odio lacus nec arcu. Nam ac posuere arcu, sit amet feugiat quam. Duis congue eleifend nisl, a bibendum dui ullamcorper ut. Aenean quam quam, porttitor at tristique vel, tristique vitae ex. Duis pretium mattis felis, a cursus nisi venenatis id. Duis nec vehicula lectus. Maecenas et nisl in ex tristique mollis.</p>
-                    </Row>
-    
-                    <Row className="m-3 auction-box">
-                    <div className="w-100 charity-tree-headers">Gift something</div>
-                        {/* Gift Options */}
-                        <Col className="p-3">
-                        <Button className="gift-option-button w-100">10 FUSD</Button>
-                        </Col>
-                        <Col className="p-3">
-                        <Button className="gift-option-button w-100">50 FUSD</Button>
-                        </Col>
-                        <Col className="p-3">
-                        <Button className="gift-option-button w-100">100 FUSD</Button>
-                        </Col>
-                    </Row>
-                    
-                </Col>
-            </Row>
-            <Row className="p-5">
-            <div className="w-100 charity-tree-headers"><h1>The wall of fame</h1></div>
+                            {/* AUCTION TIMER */}
+                            {/* <div className="w-100 charity-tree-timer">00:00:00</div> */}
+                            <Row className="auction-digits justify-content-center justify-content-md-start">
+                                <Col xs="auto" className="p-2">2 <div className="auction-dayshoursmins p-0">days</div></Col>
+                                <Col xs="auto" className="p-2">:</Col>
+                                <Col xs="auto" className="p-2">10 <div className="auction-dayshoursmins">hours</div></Col>
+                                <Col xs="auto" className="p-2">:</Col>
+                                <Col xs="auto" className="p-2">40 <div className="auction-dayshoursmins">mins</div></Col>
+                                <Col xs="auto" className="p-2">:</Col>
+                                <Col xs="auto" className="p-2">12 <div className="auction-dayshoursmins">secs</div></Col>
+                            </Row>
+
+                        <Row >
+                            <div className=" pt-4 pb-3"><h4>Neo x Flowverse Community Charity Christmas Tree </h4></div>
+                        </Row>
+                        <Row className="">
+                            {/* DESCRIPTION */}
+
+                            <div className="auction-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec lacus at dui auctor bibendum. Donec eget urna mi. Ut vel sagittis dolor. Phasellus nec neque porttitor, tincidunt tortor vel, venenatis est. Aenean maximus, tortor pulvinar iaculis tempus, velit arcu malesuada lectus, ac ullamcorper odio lacus nec arcu. Nam ac posuere arcu, sit amet feugiat quam. Duis congue eleifend nisl, a bibendum dui ullamcorper ut. Aenean quam quam, porttitor at tristique vel, tristique vitae ex. Duis pretium mattis felis, a cursus nisi venenatis id. Duis nec vehicula lectus. Maecenas et nisl in ex tristique mollis.</div>
+                        </Row>
+
+                        <Row className="mt-3">
+                            {/* AUCTION buttons */}
+                            <div className="fw-bold my-3">Current bid: <span className="current-bid-flow" > 300 FLOW</span></div>
+                            {/* <div className="fw-bold mt-3">How much would you like to bid?</div> */}
+                        </Row>
+                        <Form className="formInputs">
+                            <Row>
+                                <Col sm="12" lg="7">
+                                    <Form.Label>How much would you like to bid?</Form.Label>
+                                    <Form.Control type="number" required />
+                                </Col>
+                                <Col sm="12" lg="5" className="mt-auto">
+                                    <Button type="submit" className="w-100" variant="dark">Bid</Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row className="auction-box shadow my-5">
+                            <div className="w-100 charity-tree-headers">Gift something</div>
+                            {/* Gift Options */}
+                            <Col className="p-3">
+                                <Button className="w-100" variant="dark">10 FUSD</Button>
+                            </Col>
+                            <Col className="p-3">
+                                <Button className="w-100" variant="dark">50 FUSD</Button>
+                            </Col>
+                            <Col className="p-3">
+                                <Button className="w-100" variant="dark">100 FUSD</Button>
+                            </Col>
+                        </Row>
+                
+
+
+
+            </Container>
+            <Container style={{backgroundColor: "#F6F6F6"}} fluid>
+                <Container>
+                    <Row className="p-5">
+                    <div className="w-100 charity-tree-headers"><h1>The wall of fame</h1></div>
                     <p>Check out the wall of fame</p>
-                    </Row>
-
-
-           
-        </div>
+                </Row>
+                </Container>
+            </Container>
+            
+        </Container>
     )
 }

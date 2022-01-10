b28d2774b9cbef596347bb8d6bc5e711f01a5256
Christian Miles <christian@neomotorcycles.co.uk>
2022-01-10 22:05:22 +0000

    added last 50 entries into live feed

--- src/pages/LiveFeed.js
+++ src/pages/LiveFeed.js
@@ -6,12 +6,140 @@ import { FriendlyEventNames } from '../functions/friendlyEventNames'
 import '../components/livefeed/livefeed.css'
 import { Faq } from "../components/home/Faq";
 import ReactGA from 'react-ga'
+import axios from "axios";
 
 export default function LiveFeed() {
     document.title = ".find - live feed"
     ReactGA.pageview(window.location.pathname);
     const [latestMessage, setLatestMessage] = useState("")
+    const [eventsData, setEventsData] = useState("")
+    let limitedEventData
     //const [paintEvent, setPaintEvent] = useState([])
+
+    useEffect(() => {
+        const getEvents = async () => {
+            // console.log("getSales fired")
+            let data
+            let res = await axios
+                .get("https://prod-main-net-dashboard-api.azurewebsites.net/api/company/04bd44ea-0ff1-44be-a5a0-e502802c56d8/search?eventType=A.097bafa4e0b48eef.FIND.Sold,A.097bafa4e0b48eef.FIND.ForSale,A.097bafa4e0b48eef.FIND.AuctionCanceled,A.097bafa4e0b48eef.FIND.AuctionCanceledReservePrice,,A.097bafa4e0b48eef.FIND.AuctionStarted,A.097bafa4e0b48eef.FIND.AuctionBid,A.097bafa4e0b48eef.FIND.DirectOffer,A.097bafa4e0b48eef.FIND.DirectOfferCanceled,A.097bafa4e0b48eef.FIND.Register,A.097bafa4e0b48eef.FIND.Moved,A.097bafa4e0b48eef.FIND.SoldAuction,A.097bafa4e0b48eef.FIND.WithdrawnSale,A.097bafa4e0b48eef.FIND.ForAuction,A.097bafa4e0b48eef.FIND.DirectOfferRejected")
+            data = res.data
+            setEventsData(data)
+
+        }
+        getEvents()
+    }, [])
+
+    useEffect(() => {
+        if (eventsData !== "") {
+            // eslint-disable-next-line
+            limitedEventData = eventsData.slice(0, 50)
+            limitedEventData.forEach(events => {
+                
+            let newEvent = {}
+            let eventData = events.blockEventData
+            let eventDate = events.eventDate
+            //eslint-disable-next-line
+            let eventType = events.flowEventId.split(/\.(?=[^\.]+$)/);
+            //manage auction and offer events
+            if (eventType[1] === "AuctionCanceled" ||
+                eventType[1] === "AuctionCanceledReservePrice" ||
+                eventType[1] === "AuctionStarted" ||
+                eventType[1] === "AuctionBid" ||
+                eventType[1] === "DirectOffer" ||
+                eventType[1] === "DirectOfferCanceled") {
+                newEvent.name = eventData.name
+                newEvent.price = eventData.amount
+                //set the correct from and to
+                if (eventType[1] === "AuctionCanceled" ||
+                    eventType[1] === "AuctionCanceledReservePrice") {
+                    newEvent.from = eventData.owner
+                    newEvent.to = "-"
+                } else {
+                    newEvent.from = eventData.bidder
+                    newEvent.to = eventData.owner
+                }
+                newEvent.date = eventDate
+                newEvent.type = FriendlyEventNames[eventType[1]]
+            } else if (eventType[1] === "Register") {
+                newEvent.type = FriendlyEventNames[eventType[1]]
+                newEvent.name = eventData.name
+                let nameLength = eventData.name.length
+                if (nameLength === 3)
+                    newEvent.price = FriendlyEventNames["price3"]
+                else if (nameLength === 4)
+                    newEvent.price = FriendlyEventNames["price4"]
+                else if (nameLength >= 5)
+                    newEvent.price = FriendlyEventNames["price5"]
+                newEvent.from = ".find"
+                newEvent.to = eventData.owner
+                newEvent.date = eventDate
+            } else if (eventType[1] === "Moved" || eventType[1] === "Sold" || eventType[1] === "SoldAuction") {
+                newEvent.type = FriendlyEventNames[eventType[1]]
+                newEvent.name = eventData.name
+                newEvent.price = eventData.amount ? eventData.amount : "-"
+                newEvent.from = eventData.previousOwner
+                newEvent.to = eventData.newOwner
+                newEvent.date = eventDate
+            } else if (eventType[1] === "ForSale") {
+                if (eventData.active)
+                    newEvent.type = FriendlyEventNames[eventType[1]]
+                else
+                    newEvent.type = FriendlyEventNames["WithdrawnSale"]
+
+                newEvent.name = eventData.name
+                newEvent.price = eventData.directSellPrice
+                newEvent.from = eventData.owner
+                newEvent.to = "-"
+                newEvent.date = eventDate
+            } else if (eventType[1] === "ForAuction") {
+                if (eventData.active)
+                    newEvent.type = FriendlyEventNames[eventType[1]]
+                else
+                    newEvent.type = FriendlyEventNames["WithdrawnAuction"]
+
+                newEvent.name = eventData.name
+                newEvent.price = eventData.auctionStartPrice
+                newEvent.from = eventData.owner
+                newEvent.to = "-"
+                newEvent.date = eventDate
+            } else if (eventType[1] === "DirectOfferRejected") {
+                newEvent.type = FriendlyEventNames[eventType[1]]
+                newEvent.name = eventData.name
+                newEvent.price = eventData.amount
+                newEvent.from = eventData.owner
+                newEvent.to = eventData.bidder
+                newEvent.date = eventDate
+            } else {
+                return
+            }
+            let tableRef = document.getElementById("eventBody");
+
+            // Insert a row at the beginning of the table
+            let newRow = tableRef.insertRow(-1);
+            let newCell = newRow.insertCell(0);
+            let newText = document.createTextNode(newEvent.type);
+            newCell.appendChild(newText);
+            newCell = newRow.insertCell(1);
+            newText = document.createElement('div')
+            newText.innerHTML = "<a href='/" + newEvent.name + "' target='_blank'>" + newEvent.name + "</a>"
+            newCell.appendChild(newText);
+            newCell = newRow.insertCell(2);
+            newText = document.createTextNode(newEvent.price ? newEvent.price+" FUSD" : "-");
+            newCell.appendChild(newText);
+            newCell = newRow.insertCell(3);
+            newText = document.createTextNode(newEvent.from);
+            newCell.appendChild(newText);
+            newCell = newRow.insertCell(4);
+            newText = document.createTextNode(newEvent.to);
+            newCell.appendChild(newText);
+            newCell = newRow.insertCell(5);
+            newText = document.createTextNode(new Date(newEvent.date).toLocaleDateString() + " " + new Date(newEvent.date).toLocaleTimeString());
+            newCell.appendChild(newText);
+        })
+        }
+    }, [eventsData])
+
+
     useEffect(() => {
         if (latestMessage !== "") {
             let newEvent = {}
@@ -163,7 +291,7 @@ export default function LiveFeed() {
                         </thead>
 
                         <tbody id="eventBody">
-                            {!latestMessage &&
+                            {!latestMessage && !eventsData &&
                                 <tr>
                                     <td colSpan={6} align="center">Waiting for feed data</td>
                                 </tr>
@@ -177,6 +305,7 @@ export default function LiveFeed() {
                 <div align="right" className="pe-2"><a href="https://graffle.io" target="_blank" rel="noreferrer"><Image src="/assets/img/livefeed/powered-by-graffle.webp" style={{maxHeight: "44px"}} fluid></Image></a></div>
             </div>
             <div className="my-5 mx-auto"></div>
+            {JSON.stringify(limitedEventData)}
             <Faq />
         </Container>
     )

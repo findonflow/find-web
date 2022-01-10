bf1071c6bf3e51ebb9f947d3b3b7f2b68fc1ee3f
Christian Miles <christian@neomotorcycles.co.uk>
2022-01-10 23:06:03 +0000

    fix price for moved names on live feed

--- src/pages/LiveFeed.js
+++ src/pages/LiveFeed.js
@@ -124,7 +124,7 @@ export default function LiveFeed() {
             newText.innerHTML = "<a href='/" + newEvent.name + "' target='_blank'>" + newEvent.name + "</a>"
             newCell.appendChild(newText);
             newCell = newRow.insertCell(2);
-            newText = document.createTextNode(newEvent.price ? newEvent.price+" FUSD" : "-");
+            newText = document.createTextNode(newEvent.price !== "-" ? newEvent.price+" FUSD" : "-");
             newCell.appendChild(newText);
             newCell = newRow.insertCell(3);
             newText = document.createTextNode(newEvent.from);

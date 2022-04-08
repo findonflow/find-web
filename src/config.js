ad314fa1937eefedf3c9da34ddcf42a248b6b3b7
christian <christian@neomotocycles.co.uk>
2022-04-08 10:43:10 +0100

    update to fcl v1 alpha

--- src/config.js
+++ src/config.js
@@ -1,10 +1,15 @@
 import { config } from "@onflow/fcl"
-
+import {send as grpcSend} from "@onflow/transport-grpc"
+//import {send as httpSend} from "@onflow/transport-http"
 
 config()
   .put("accessNode.api", process.env.REACT_APP_ACCESS_NODE) // Configure FCL's Access Node
-  .put("challenge.handshake", process.env.REACT_APP_WALLET_DISCOVERY) // Configure FCL's Wallet Discovery mechanism
+  //.put("challenge.handshake", process.env.REACT_APP_WALLET_DISCOVERY) // Configure FCL's Wallet Discovery mechanism
+  .put("discovery.wallet", process.env.REACT_APP_WALLET_DISCOVERY)
+  .put("app.detail.title", ".find")
+  .put("app.detail.icon", "https://find.xyz/find.png")
   .put("env", process.env.REACT_APP_NETWORK)
+  .put("sdk.transport", grpcSend)
   .put("decoder.Enum", (val) => {
     const result = {
       type: val.id
